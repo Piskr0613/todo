e6d915dac75cc5e598fecc712e7a5845a4fbd2e6
zd <2804239455@qq.com>
2023-04-30 11:23:59 +0800

    second commit

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -5,18 +5,28 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.todo.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+typealias AdapterData=RvAdapter.Data
 class MainActivity : AppCompatActivity() {
 
     private val mBinding: ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val data= listOf(
+        AdapterData("写作业"),
+        AdapterData("吃药")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
+        mBinding.rv.apply {
+            layoutManager=LinearLayoutManager(this@MainActivity)
+            adapter=RvAdapter(data)
+        }
     }
 }
---
+++ app/src/main/java/com/example/todo/RvAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.todo
+
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RvAdapter(private val data:List<Data>): RecyclerView.Adapter<RvAdapter.ViewHolder>() {
+    data class Data(val assignment:String)
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val textView=view.findViewById<TextView>(R.id.tv_assignment)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.text,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val itemData=data[position]
+        holder.apply {
+            textView.text=itemData.assignment
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
