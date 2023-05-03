75c903ebb9a47f31ee6f8f5a1e2ad2e2ec3a03d4
Piskr0613 <119737732+Piskr0613@users.noreply.github.com>
2023-05-03 10:24:48 +0800

    Update LoginActivity.kt

--- app/src/main/java/com/example/todo/LoginActivity.kt
+++ app/src/main/java/com/example/todo/LoginActivity.kt
@@ -1,13 +1,10 @@
 package com.example.todo
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.todo.databinding.ActivityLoginBinding
-import com.example.todo.databinding.ActivityMainBinding
-import android.content.SharedPreferences
+
 
 class LoginActivity :SharedPreferencesActivity(){
     private val mBinding by lazy{
@@ -20,6 +17,7 @@ class LoginActivity :SharedPreferencesActivity(){
         mBinding.login.setOnClickListener {
             val account=mBinding.username.text.toString()
             val password=mBinding.password.text.toString()
+            //检验密码是否正确
             if(login(account,password)){
                 Toast.makeText(this,"登录成功",Toast.LENGTH_SHORT).show()
                 val intent=Intent(this,MainActivity::class.java)
@@ -28,6 +26,7 @@ class LoginActivity :SharedPreferencesActivity(){
                 Toast.makeText(this,"账号或密码错误",Toast.LENGTH_SHORT).show()
             }
         }
+        //注册界面跳转
         mBinding.register.setOnClickListener {
             val intent= Intent(this,RegisterActivity::class.java)
             startActivity(intent)
